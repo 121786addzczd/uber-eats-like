bb4fcbb6111615fe53441e3b5e78ab4599966093
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-03-09 10:21:55 +0900

    APIのデータをreducerを通して画面に表示する

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,18 +1,46 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 
+// reducers
+import {
+  initialState as foodsInitialState,
+  foodsActionTypes,
+  foodsReducer,
+} from '../reducers/foods';
+
+// apis
 import { fetchFoods } from '../apis/foods';
-
+// constants
+import { REQUEST_STATE } from '../constants';
 export const Foods = ({match}) => {
 
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
   useEffect(() => {
+    dispatch({ type: foodsActionTypes.FETCHING });
     fetchFoods(match.params.restaurantsId)
-    .then((data) =>
-      console.log(data)
-    )
+      .then((data) => {
+        dispatch({
+          type: foodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      })
   }, [])
   return (
     <>
-      フード一覧
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <>
+            <p>ロード中</p>
+          </>
+        :
+        foodsState.foodsList.map(food =>
+          <div key={food.id}>
+            {food.name}
+          </div>
+        )
+      }
     </>
   )
 }
