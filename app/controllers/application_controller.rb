299c86bed6d37fe1f6f951ddbbf365f737ad5481
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-03-09 09:17:40 +0900

    フード一覧のAPIを叩く
    React Routerではmatchオブジェクトを通じてURLに含まれるidを取得することができる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :fake_load
+
+  # 1秒だけプログラムの実行を止めてSPAぽさをローカル環境であえてだす
+  def fake_load
+    sleep(1)
+  end
+
 end
--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,10 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+import { fetchFoods } from '../apis/foods';
 
 export const Foods = ({match}) => {
+
+  useEffect(() => {
+    fetchFoods(match.params.restaurantsId)
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
   return (
     <>
       フード一覧
-      <p>restaurantsIdは {match.params.restaurantsId} です</p>
     </>
   )
 }
